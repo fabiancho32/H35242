d5c32d3983e9b7fbcea632ce2a27ce7402a30b28
cherman11 <valencia9405@gmail.com>
2018-03-30 22:14:03 -0500

    Cambios Fase 2, interfaz

--- src/InterfazPrincipal.java
+++ src/InterfazPrincipal.java
@@ -12,6 +12,12 @@ import javax.swing.JTextPane;
 import javax.swing.JTextArea;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 public class InterfazPrincipal extends JFrame {
 
@@ -37,23 +43,64 @@ public class InterfazPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public InterfazPrincipal() {
+		
+		
+		
+		setTitle("COMPILADOR HUQ");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JTree tree = new JTree();
-		tree.setBounds(325, 5, 112, 263);
+		tree.setBounds(565, 50, 205, 490);
 		contentPane.add(tree);
 		
 		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(32, 153, 287, 115);
+		textArea_1.setBounds(32, 447, 521, 93);
 		contentPane.add(textArea_1);
 		
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 770, 26);
+		contentPane.add(menuBar);
+		
+		JMenu mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		JMenuItem mntmAbrir = new JMenuItem("Abrir");
+		mnArchivo.add(mntmAbrir);
+		
+		JMenuItem mntmNuevo = new JMenuItem("Nuevo");
+		mnArchivo.add(mntmNuevo);
+		
+		JMenuItem mntmGuardar = new JMenuItem("Guardar");
+		mnArchivo.add(mntmGuardar);
+		
+		JMenuItem mntmGuardarComo = new JMenuItem("Guardar Como");
+		mnArchivo.add(mntmGuardarComo);
+		
+		JMenu mnCompilar = new JMenu("Compilar");
+		menuBar.add(mnCompilar);
+		
+		JMenuItem mntmIniciarCompilacion = new JMenuItem("Iniciar Compilacion");
+		mnCompilar.add(mntmIniciarCompilacion);
+		
+		JLabel lblConsola = new JLabel("Consola");
+		lblConsola.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblConsola.setForeground(Color.BLACK);
+		lblConsola.setBounds(32, 415, 97, 19);
+		contentPane.add(lblConsola);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(26, 39, 527, 363);
+		contentPane.add(scrollPane);
+		
 		JTextArea textArea = new JTextArea();
-		textArea.setBounds(32, 11, 287, 135);
-		contentPane.add(textArea);
+		scrollPane.setViewportView(textArea);		
+		TextLineNumber tln = new TextLineNumber(textArea);
+		scrollPane.setRowHeaderView(tln);
 	}
 }
