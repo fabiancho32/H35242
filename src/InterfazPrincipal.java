6e5dd50c7afe9eebea233b990a231f5a40afc5e9
fabiancho32 <the-fabiancho@hotmail.com>
2018-04-05 23:52:49 -0500

    terminado guarda y sobreescribe

--- src/InterfazPrincipal.java
+++ src/InterfazPrincipal.java
@@ -101,16 +101,35 @@ public class InterfazPrincipal extends JFrame {
 		mnArchivo.add(mntmGuardarComo);
 		
 		JMenuItem mntmGuardar = new JMenuItem("Guardar");
+		mntmGuardar.setEnabled(false);
 		mntmGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				guardar=true;
-				if(fFichero!=null){
-					modificar(fFichero,cadena,textAreaCodigo.getText());
-				}else{
-					mntmGuardar.getAction();
+					borrar(fFichero);
+					File archivo = fFichero;
+					if (!archivo.getName().endsWith("huq")) {
+						archivo = new File(archivo.getAbsolutePath());
+					}
+					FileWriter escritor = null;
+					try {
+						escritor = new FileWriter(archivo);
+						escritor.write(textAreaCodigo.getText());
+					} catch (FileNotFoundException ex) {
+						Logger.getLogger(InterfazPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (IOException ex) {
+						Logger.getLogger(InterfazPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+					} finally {
+						try {
+							escritor.close();
+						} catch (IOException ex) {
+							Logger.getLogger(InterfazPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+						}
+					}
+
 				}
-				//falta el codigo que sobreescriba el archivo que ya esta cargador 
-			}
+			
+		
+			
 		});
 		mnArchivo.add(mntmGuardar);
 
@@ -234,6 +253,7 @@ public class InterfazPrincipal extends JFrame {
 			
 				if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(null)) {
 					File archivo = fileChooser.getSelectedFile();
+					System.out.println(archivo.getAbsolutePath());
 
 					if (archivo.getName().endsWith("huq")) {
 						FileReader lector = null;
@@ -252,10 +272,9 @@ public class InterfazPrincipal extends JFrame {
 							}
 
 							// Pone el contenido del fichero en el area de texto
-							   textAreaCodigo.setText(contenidoFichero.toString());
-							   
-							   fFichero= archivo;
-							   cadena=textAreaCodigo.getText();
+							   textAreaCodigo.setText(contenidoFichero.toString());							   
+							   fFichero= new File(archivo.getAbsolutePath());
+							   mntmGuardar.setEnabled(true);
 							////////////////////////////////////////////////////
 
 						} catch (FileNotFoundException ex) {
@@ -300,7 +319,7 @@ public class InterfazPrincipal extends JFrame {
 					if (!archivo.getName().endsWith("huq")) {
 						archivo = new File(fileChooser.getSelectedFile() + ".huq");
 						fFichero= archivo;
-						cadena= textAreaCodigo.getText();
+						mntmGuardar.setEnabled(true);
 					}
 					FileWriter escritor = null;
 					try {
@@ -336,99 +355,6 @@ public class InterfazPrincipal extends JFrame {
 	public JTree getTree() {
 		return tree;
 	}
-	  void Escribir(File fFichero,String cadena)
-	    {
-	        // Declaramos un buffer de escritura
-	        BufferedWriter bw;
-
-	        try
-	        {
-	            // Comprobamos si el archivo no existe y si es asi creamos uno nuevo.
-	         if(!fFichero.exists())
-	         {
-	             fFichero.createNewFile();
-	         }
-
-	           // Luego de haber creado el archivo procedemos a escribir dentro de el.
-	            bw = new BufferedWriter(new FileWriter(fFichero,true));
-	            bw.write(cadena);
-	            bw.close();
-
-	        }catch(Exception e)
-	        {
-	            System.out.println(e);
-	        }
-
-	    }
-	  void modificar(File fAntiguo,String aCadena,String nCadena)
-	    {
-	       /*
-	            Las dos lienas de codigo siguientes, basicamente lo que hacen es generar un numero aleatorio y
-	            asignarselos a una nueva variable "nFnuevo" (Nombre Fichero Nuevo) la cual es igual a la ruta
-	            del directorio padre "fAntiguo" mas  la palabra auxiliar seguido del numero aletorio y la extension
-	            del archivo nuevo
-	       * */
-	        Random numaleatorio = new Random(3816L);
-	        String nFnuevo = fAntiguo.getParent()+"/auxiliar"+String.valueOf(Math.abs(numaleatorio.nextInt()))+".txt";
-
-	     // Creo un nuevo archivo
-	        File fNuevo= new File(nFnuevo);
-	        // Declaro un nuevo buffer de lectura
-	        BufferedReader br;
-	        try
-	        {
-	            /*Valido si el fichero antiguo que pasamos como parametro existe, si es asi procedo a leer el
-	            contenido que hay dentro de el
-	             */
-
-	            if(fAntiguo.exists())
-	            {
-	                br = new BufferedReader(new FileReader(fAntiguo));
-
-	                String linea;
-
-	                /* Mientras el contenido del archivo sea diferente de null procedo a comprar  la linea a modificar con
-	                lo que hay dentro del archivo, si linea es igual a aCadena escribo el contenido de aCadena en mi nuevo
-	                fichero(Auxiliar) de lo contrario escribo el mismo contenido que ya tenia el antiguo fichero en mi fichero auxiliar
-
-	                 */
-	                while((linea=br.readLine()) != null)
-	                {
-	                    if(linea.equals(aCadena))
-	                    {
-	                        Escribir(fNuevo,nCadena);
-
-	                    }
-	                    else
-	                    {
-	                        Escribir(fNuevo,linea);
-	                    }
-	                }
-
-	              // Cierro el buffer de lectura
-	                br.close();
-
-	                // Capturo el nombre del fichero antiguo
-	                String nAntiguo = fAntiguo.getName();
-	                // Borro el fichero antiguo
-	                borrar(fAntiguo);
-	                //Renombro el fichero auxiliar con el nombre del fichero antiguo
-	                fNuevo.renameTo(fAntiguo);
-
-
-
-
-	            }
-	            else
-	            {
-	                System.out.println("Fichero no Existe");
-	            }
-
-	        }catch(Exception e)
-	        {
-	            System.out.println(e);
-	        }
-	    }
 
 
 void borrar (File Ffichero)
